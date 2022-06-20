868eab1c7a2803210f7bc9f44fe012d1b6ec52d2
lamalytics <95007715+lamalytics@users.noreply.github.com>
2022-06-19 20:19:54 -0700

    6. Deploy Contracts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,32 @@
+const { ethers } = require("hardhat");
+
 // scripts/deploy.js
 async function main() {
     // We get the contract to deploy
-    const Bid = await ethers.getContractFactory('Bid');
+    const token = await ethers.getContractFactory('AEXToken');
+    console.log('Deploying token...');
+    const Token = await token.deploy(ethers.utils.parseUnits("10000", "ether"));
+    await Token.deployed();
+    console.log('Token deployed to: ', Token.address);
+
+    const swap = await ethers.getContractFactory('Swap');
+    console.log('Deploying Swap...');
+    const Swap = await swap.deploy(Token.address);
+    await Swap.deployed();
+    console.log('Swap deployed to: ', Swap.address);
+
+    const bid = await ethers.getContractFactory('Bid');
     console.log('Deploying Bid...');
-    const bid = await Bid.deploy(1);
-    await bid.deployed();
-    console.log('Bid deployed to:', bid.address);
+    const Bid = await bid.deploy(100);
+    await Bid.deployed();
+    console.log('Bid deployed to:', Bid.address);
 }
+
+
 
 main()
     .then(() => process.exit(0))
     .catch(error => {
         console.error(error);
         process.exit(1);
-    });
+    }); 
