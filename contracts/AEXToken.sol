a4d8191d444f87aea6f4ff80823840be44bf6f1b
lamalytics <95007715+lamalytics@users.noreply.github.com>
2022-06-19 20:18:55 -0700

    2. ERC-20 Token Creation

--- contracts/AEXToken.sol
+++ contracts/AEXToken.sol
@@ -3,9 +3,19 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract AEXToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("AdExchange", "AEX") {
-        _mint(msg.sender, initialSupply);
+contract AEXToken is ERC20, Ownable {
+
+    constructor(uint256 _initialSupply) ERC20("AdExchange", "AEX") {
+        _mint(address(this), _initialSupply);
     }
+    /*
+    @ dev new minting for external swap contract
+    */
+
+    function mint(address account, uint amount) external onlyOwner {
+        _mint(account, amount);
+    }
+    
 }
