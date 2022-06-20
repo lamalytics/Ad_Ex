fea079c2ca3b92bd4b960a7e1a9565e6ede28419
lamalytics <95007715+lamalytics@users.noreply.github.com>
2022-06-19 20:19:27 -0700

    4. Bid for Ads

--- contracts/Bid.sol
+++ contracts/Bid.sol
@@ -4,51 +4,46 @@ pragma solidity ^0.8.0;
 
 // Import Auth from the access-control subdirectory
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "./AEXToken.sol";
+import "./Swap.sol";
 
-contract Bid is Ownable{
-
+contract Bid is Ownable {
+    // AEX represents the token smart contract
+    Swap public swap;
+    AEXToken public token;
     // fire when newAdvertiser is registered
     event newAdvertiser(address addr);
 
     // fire when new ad is placed
     event newAd(uint256 adId, uint256 value);
 
-    struct Advertiser {
-        address id;
-        uint256 bidCount;
-        bool isRegistered;
-    }
-    
-    Advertiser[] public advertisers;
-    mapping(address => Advertiser) adDetails;
+    // fire when funds transferred
+    event Transfer(address from, address to, uint256 value);
+    // event Approval(owner, spender, value);
+
+    mapping(address => bool) isRegistered;
 
     // initialize bidCount to 0
-    uint bidCount = 0;
+    uint public bidCount = 0;
 
-    uint curBid;
+    uint256 curBid;
     // sets the bid floor bid passed in param
-    constructor(uint256 _curBid) {
+    // TO USE when swapping tokens
+    constructor(uint256 _curBid, Swap _swap) {
         curBid = _curBid;
+        swap = _swap;
+        token = swap.token();
     }
     
     
     // register advertiser for bidding (onlyOwner + admin[later])
     function register(address _address) public onlyOwner {
-        // loops through advertisers array to see if address already exists already
-        for(uint i = 0; i < advertisers.length; i++) {
-            // registers address if address is new
-            if(_address != advertisers[i].id) {
-                advertisers.push(Advertiser({
-                    id: _address,
-                    bidCount: 0,
-                    isRegistered: true
-                }));
-                // new Advertiser has registered
-                
-                emit newAdvertiser(_address);
-            }
-        }
-        // else revert and error to say that the advertiser has already been registered
+        require(token.balanceOf(_address) > 0, "please buy AEX tokens first");
+        require(_address != owner(), "The owner of this contract cannot register");
+        require(isRegistered[_address] == false, "address is already registered");
+        isRegistered[_address] = true;
+        emit newAdvertiser(_address);
         // fallback for nonvalid addresses 
     }
 
@@ -59,33 +54,36 @@ contract Bid is Ownable{
         // fires event
         // mapping of address to ad id
 
-    function placeBid (uint256 _value, Advertiser memory _advertiser) public {
-        require(_value > curBid, "please bid above current bid");
-        require(_advertiser.isRegistered == true, "please register your address");
-        // transfer ad space 
-        // call send transaction to contract address wallet
 
+    function placeBid (uint256 _value) public {
+        // require(token.balanceOf(msg.sender) > 0, "please buy AEX tokens first");
+        require(isRegistered[msg.sender] == true, "please register your address");
+        require(token.balanceOf(msg.sender) >= _value, "you do not have enough tokens");
+        // can be replaced with msg.value
+        require(_value > curBid, "please bid above current bid");
+        
+        // transfer ad space 
+        // call send transaction to contract address wallet (look at swap.sellTokens())
+        // requires approval to send tokens to this contract
+        token.transferFrom(msg.sender,address(this), _value);
+        // swap.sellTokens(_value);
+        emit Transfer(msg.sender, address(this), _value);
         // incremement bid count which is the ad ID
         bidCount++;
+        curBid = _value;
         emit newAd(bidCount, _value);
     }
     
+    function currentBid() public view returns (uint256) {
+        return curBid;
+    }
 
-    
-    // transfer of funds to contract address if bid is successful
-    // do not allow transaction if bid is lower or equal
+    function getToken() public view returns(address) {
+        return address(token);
+    }
 
+    function getSwap() public view returns(address) {
+        return address(swap);
+    }
 
-
-    // function store(uint256 value) public onlyOwner {
-    //     // Require that the caller is registered as an administrator in Auth
-    //     // require(_auth.isAdministrator(msg.sender), "Unauthorized");
-
-    //     _value = value;
-    //     emit newBid(value);
-    // }
-
-    // function retrieve() public view returns (uint256) {
-    //     return _value;
-    // }
 }
